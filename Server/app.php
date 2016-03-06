a2d4aa5c740e5b5ea53adf0033a7fed922192e2f
fireandrew <fire-andrew@hotmail.com>
2016-03-06 17:43:12 +0200

    implement post function

--- Server/app.php
+++ Server/app.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Andreas
- * Date: 26/2/2016
- * Time: 23:24
- */
+
+if (isset($_POST)) {
+    if (empty($_POST['username'])) {
+        echo "Error , no username specified";
+    } else if (empty($_POST['password'])) {
+        echo "Error , no password specified";
+    } else {
+        $data = "{username:" . $_POST['username'] .
+            ",password:" . $_POST['password'] . "}";
+        header('Content-Type: application/json');
+        echo json_encode($data);
+    }
+} else {
+    echo "Error , post is not set";
+}
+
