b6b1843eb683b52ff6783e704370f6c28df031ac
fireandrew <fire-andrew@hotmail.com>
2016-03-06 17:44:05 +0200

    implement ajax post

--- index.php
+++ index.php
@@ -3,9 +3,18 @@
 <head lang="en">
     <meta charset="UTF-8">
     <title>Home Deco</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="FrontEnd/js/app.js"></script>
 </head>
 <body>
-    <h1>Welcome to Home Deco</h1>
-    <?php require_once("FrontEnd/templates/form.php") ?>
+
+
+<form action="Server/app.php">
+    Name: <input type="text" name="name" value="andreas"><br>
+    E-mail: <input type="text" name="email"><br>
+    <button onclick="run()">click</button>
+</form>
+<div id="myDiv"></div>
+
 </body>
 </html>
